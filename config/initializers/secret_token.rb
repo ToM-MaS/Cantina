3accea3644329e3d98241a56aa5a4ab66b41f90d
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2011-01-11 18:44:08 +0100

    Basic framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cantina::Application.config.secret_token = 'da1b224ec8762f2fe131283e72070c4e4ce017d8f502892c38817021221288b626cc9cf55aea0de4784681eafd7c0b8002654005b1c22a16358eaa2158e87ce5'

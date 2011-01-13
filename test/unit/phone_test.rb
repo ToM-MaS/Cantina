0c24ab0553b49ee61a2e0e284f0b161657d547f4
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2011-01-13 19:06:59 +0100

    Added tests for mac addresses.

--- test/unit/phone_test.rb
+++ test/unit/phone_test.rb
@@ -4,4 +4,47 @@ class PhoneTest < ActiveSupport::TestCase
   should "be valid" do
     assert Factory.build(:phone).valid?
   end
+  
+  # Test not valid mac addresses
+  #
+  should "not be valid" do
+    assert !Factory.build(:phone, :mac_address => nil).valid?
+  end
+
+  should "not be valid" do
+    assert !Factory.build(:phone, :mac_address => '007').valid?
+  end
+
+  should "not be valid" do
+    assert !Factory.build(:phone, :mac_address => 'AAFF0011').valid?
+  end
+
+  should "not be valid" do
+    assert !Factory.build(:phone, :mac_address => '1122334455GG').valid?
+  end
+  
+  should "not be valid" do
+    assert !Factory.build(:phone, :mac_address => '11:22:33:aa:bb:cc:').valid?
+  end
+
+  should "not be valid" do
+    assert !Factory.build(:phone, :mac_address => '112233aabbcc11').valid?
+  end
+  
+  
+  # Test valid mac addresses
+  #
+  should "be valid" do
+    assert Factory.build(:phone, :mac_address => '11:22:33:aa:bb:cc').valid?
+  end
+
+  should "be valid" do
+    assert Factory.build(:phone, :mac_address => '11:22:33:AA:BB:cc').valid?
+  end
+
+  should "be valid" do
+    assert Factory.build(:phone, :mac_address => '112233AABBcc').valid?
+  end
+
+  
 end

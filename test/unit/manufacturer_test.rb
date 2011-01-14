c8445315f1d0cbcda9b6f0af601ea5f3dbe04189
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2011-01-14 10:51:34 +0100

    Unique test for manufacturer.

--- test/unit/manufacturer_test.rb
+++ test/unit/manufacturer_test.rb
@@ -4,4 +4,11 @@ class ManufacturerTest < ActiveSupport::TestCase
   should "be valid" do
     assert Factory.build(:manufacturer).valid?
   end
+  
+  # A manufacturer name must be unique
+  should "not be valid with not unique name" do
+    manufacturer = Factory.create(:manufacturer)
+    assert !Factory.build(:manufacturer, :name => manufacturer.name).valid?
+  end
+  
 end

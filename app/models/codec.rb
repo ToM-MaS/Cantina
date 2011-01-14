9fae9b31332dd7e1b7ce0e64f1784ae138dd702b
pk (amooma) <philipp.kempgen@amooma.de>
2011-01-14 11:09:22 -0800

    validate Codec name

--- app/models/codec.rb
+++ app/models/codec.rb
@@ -13,6 +13,8 @@ class Codec < ActiveRecord::Base
   has_many :sip_account_codecs, :dependent => :destroy
   has_many :sip_accounts, :through => :sip_account_codecs
   
+  #TODO: Validations
+  
   validates_presence_of :name
-  validates_format_of   :name, :with => /^[a-zA-Z0-9.\-_]{1,40}$/, :allow_nil => false, :allow_blank => false
+  #validates_format_of   :name, :with => /^[a-zA-Z0-9.\-_]{1,40}$/, :allow_nil => false, :allow_blank => false
 end

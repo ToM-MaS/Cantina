f1fd1f233cd56eed78552ee655187c42b2da21fd
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-01-17 14:47:58 +0100

    mehr Tests

--- test/unit/sip_account_test.rb
+++ test/unit/sip_account_test.rb
@@ -62,18 +62,28 @@ class SipAccountTest < ActiveSupport::TestCase
   
   # Some tests for valid registrar
   #
-  [ '10.0.0.0', 'www.amooma.de', '2001:0db8:85a3:0000:0000:8a2e:0370:7334'].each do |valid_registrar|
+  [
+    '10.0.0.0',
+    'www.amooma.de',
+    '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
+    'example.com123',
+  ].each { |valid_registrar|
     should "be possible to set registrar to #{valid_registrar}" do
       assert Factory.build(:sip_account, :registrar => valid_registrar).valid?
     end
-  end
+  }
   
   # Some tests for invalid registrar
   #
-#  [ '10.0.x.0', 'www.amooma.01', '2001:0xb8:85a3:0000:0000:8a2e:0370:7334'].each do |invalid_registrar|
-#    should "not be possible to set registrar to #{invalid_registrar}" do
-#      assert !Factory.build(:sip_account, :registrar => invalid_registrar).valid?
-#    end
-#  end
+  [
+    '10.0.x.0',
+    'www.amooma.01',
+    '2001:0xb8:85a3:0000:0000:8a2e:0370:7334',
+    'example.123com',
+  ].each { |invalid_registrar|
+    should "not be possible to set registrar to #{invalid_registrar}" do
+      assert !Factory.build(:sip_account, :registrar => invalid_registrar).valid?
+    end
+  }
 
 end

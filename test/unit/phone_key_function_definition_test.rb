ce67201aa15c8b18b717fee4a2a6d1fcb8f8f8f9
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-01-18 18:26:37 +0100

    more tests for the PhoneKeyFunctionDefinition model

--- test/unit/phone_key_function_definition_test.rb
+++ test/unit/phone_key_function_definition_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class PhoneKeyFunctionDefinitionTest < ActiveSupport::TestCase
+  
   should "be valid" do
     assert Factory.build(:phone_key_function_definition).valid?
   end
+  
+  
+  # Test valid type_of_class
+  [
+    'string',
+    'integer',
+    'boolean',
+    'url',
+    'uri',
+  ].each do |type_of_class|
+    should "allow #{type_of_class.inspect} as type_of_class" do
+      assert Factory.build( :phone_key_function_definition, :type_of_class => type_of_class ).valid?
+    end
+  end
+  
+  # Test invalid type_of_class
+  [
+    'String',
+    'Str',
+    'str',
+    'Integer',
+    'Int',
+    'int',
+    'FixNum',
+    'Fixnum',
+    'fixnum',
+    'Boolean',
+    'Bool',
+    'bool',
+    'Url',
+    'URL',
+    'Uri',
+    'URI',
+    '',
+    nil,
+    'Object',
+  ].each do |type_of_class|
+    should "not allow #{type_of_class.inspect} as type_of_class" do
+      assert ! Factory.build( :phone_key_function_definition, :type_of_class => type_of_class ).valid?
+    end
+  end
+  
+  
 end

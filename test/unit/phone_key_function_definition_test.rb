5625f4f6f9df8795724ef44b46bfc42cef5224c0
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-01-18 20:47:40 +0100

    test PhoneKeyFunctionDefinition.regex_validation

--- test/unit/phone_key_function_definition_test.rb
+++ test/unit/phone_key_function_definition_test.rb
@@ -48,4 +48,54 @@ class PhoneKeyFunctionDefinitionTest < ActiveSupport::TestCase
   end
   
   
+  # Test valid regex_validation
+  [
+    nil,
+    '',
+    //                .to_s,  # "(?-mix:)"
+    /^$/              .to_s,  # "(?-mix:^$)"
+    /^ .* $/x         .to_s,  # "(?x-mi:^ .* $)"
+    /[\\x00]/         .to_s,
+    /[\\u0000]/       .to_s,
+    /[\\u{00}]/       .to_s,
+    /[\\u{000000}]/   .to_s,
+    /[\\u{10ffff}]/   .to_s,
+  ].each { |regex_validation|
+    should "allow #{regex_validation.inspect} as regex_validation" do
+      assert Factory.build(
+        :phone_key_function_definition,
+        :regex_validation => regex_validation
+      ).valid?
+    end
+  }
+  
+  # Test invalid regex_validation
+  [
+    '\\',             # too short escape sequence
+    '\\(?-mix:)',     # unmatched close parenthesis
+    '(',              # end pattern with unmatched parenthesis
+    '(?-mix:^$',      # end pattern with unmatched parenthesis
+    '[',              # premature end of char-class
+    '{0,2}',          # target of repeat operator is not specified
+    '*',              # target of repeat operator is not specified
+    '?',              # target of repeat operator is not specified
+    '[]',             # empty char-class
+    '[\\x]',          # invalid hex escape
+    '[z-a]',          # empty range in char class
+    '[\\u00]',        # invalid Unicode escape
+    '[\\u{FFFFFF}]',  # invalid Unicode range
+    '[\\u{110000}]',  # invalid Unicode range
+    '[\\u{0000000}]', # invalid Unicode range
+    '[\u{00-}]',      # invalid Unicode list
+    '[\u{}]',         # invalid Unicode list
+  ].each { |regex_validation|
+    should "not allow #{regex_validation.inspect} as regex_validation" do
+      assert ! Factory.build(
+        :phone_key_function_definition,
+        :regex_validation => regex_validation
+      ).valid?
+    end
+  }
+  
+  
 end

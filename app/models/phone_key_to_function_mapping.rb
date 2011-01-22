8ef5858a7f84b62a7039a1ae92f58aa8dd1dea98
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-01-22 15:41:40 +0100

    ./script/annotate (annotate-models)

--- app/models/phone_key_to_function_mapping.rb
+++ app/models/phone_key_to_function_mapping.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+# Schema version: 20110121121114
+#
+# Table name: phone_key_to_function_mappings
+#
+#  id                               :integer         not null, primary key
+#  phone_model_key_id               :integer
+#  phone_key_function_definition_id :integer
+#  created_at                       :datetime
+#  updated_at                       :datetime
+#
+
 class PhoneKeyToFunctionMapping < ActiveRecord::Base
   belongs_to :phone_key_function_definition
   belongs_to :phone_mode_key

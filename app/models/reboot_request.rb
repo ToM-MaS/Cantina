0cb6e713522be30ec2a0e20d4eefe4b6c36fa06e
Peter Kozak <spag@carcharoth.(none)>
2011-01-23 12:58:34 +0100

    Reboot function for Aastra phones added

--- app/models/reboot_request.rb
+++ app/models/reboot_request.rb
@@ -23,12 +23,19 @@ class RebootRequest < ActiveRecord::Base
     if (phone.class != Phone)
       return false
     end
- 
-    success = reboot_phone_by_http(:ip_address => phone.ip_address, :path => 'confirm.htm?REBOOT=yes', :user => phone.http_user, :password => phone.http_password)
+    
+    if (phone.phone_model.manufacturer.ieee_name == "SNOM Technology AG")
+      success = reboot_phone_by_http(:ip_address => phone.ip_address, :path => 'confirm.htm?REBOOT=yes', :user => phone.http_user, :password => phone.http_password)
+    elsif (phone.phone_model.manufacturer.ieee_name == "DeTeWe-Deutsche Telephonwerke")
+      success = reboot_phone_by_http(:ip_address => phone.ip_address, :path => 'logout.html', :user => phone.http_user, :password => phone.http_password)
+      success = reboot_phone_by_http(:ip_address => phone.ip_address, :path => 'logout.html', :user => phone.http_user, :password => phone.http_password)
+      success = reboot_phone_by_http(:ip_address => phone.ip_address, :path => 'reset.html', :post => {"resetOption" => "0"},:user => phone.http_user, :password => phone.http_password)
+    end
+    
     return success
   end
   
-  def self.reboot_phone_by_http(options = {:ip_address => nil, :port => 80, :path => nil, :user => nil, :password => nil, :ssl => false, :timeout => 5})
+  def self.reboot_phone_by_http(options = {:ip_address => nil, :port => 80, :path => nil, :post => nil, :user => nil, :password => nil, :ssl => false, :timeout => 5})
     require 'net/http'
     
     http = Net::HTTP.new(options[:ip_address], options[:port])
@@ -36,10 +43,15 @@ class RebootRequest < ActiveRecord::Base
       http.use_ssl = options[:ssl]
       http.open_timeout = options[:timeout]
       http.read_timeout = options[:timeout]
-      request = Net::HTTP::Get.new(options[:path], nil)
+      if (options[:post])
+	request = Net::HTTP::Post.new(options[:path], nil)
+	request.set_form_data(options[:post])
+      else
+	request = Net::HTTP::Get.new(options[:path], nil)
+      end
       request.basic_auth(options[:user], options[:password])
       response = http.request(request)
-      
+
       # TODO Proper exception handling, more response codes
       success = case response.code
 	when "200" then true

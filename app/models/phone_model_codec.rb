6f7a09e468ac3f71a2f97bcb85ec577aab81d6ba
Sascha Daniels <sd@alternative-solution.de>
2011-01-28 12:27:13 +0100

    Validations

--- app/models/phone_model_codec.rb
+++ app/models/phone_model_codec.rb
@@ -17,5 +17,15 @@ class PhoneModelCodec < ActiveRecord::Base
   
   acts_as_list :scope => :phone_model
 
-  # TODO validation
+
+  validates_presence_of(:phone_model_id, :message => "Need phone_model_id")
+  validates_numericality_of(:phone_model_id, :message => "Only integer allowed as phone_model_id")
+  validates_presence_of(:codec_id, :message => "Need codec_id")
+  validates_numericality_of(:codec_id, :message => "Only integer allowes as codec_id")
+  validate :codec_exists
+  def codec_exists
+    if !Codec.all.collect {|c| c.id}.include? self.codec_id
+      errors.add(:codec_id, "#{self.codec_id} is not a valid codec_id")
+    end
+  end
 end

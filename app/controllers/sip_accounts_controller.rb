44d59517a3c8606940139551e27a87e9a250f940
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2011-01-30 22:18:21 +0100

    /phone_models/3/phones lists only phones from the phone_model 3.

--- app/controllers/sip_accounts_controller.rb
+++ app/controllers/sip_accounts_controller.rb
@@ -2,10 +2,10 @@ class SipAccountsController < ApplicationController
   # GET /sip_accounts
   # GET /sip_accounts.xml
   def index
-    if !params[:phone_id].blank?
-      @sip_accounts = Phone.find(params[:phone_id]).sip_accounts
-    else
+    if params[:phone_id].blank?
       @sip_accounts = SipAccount.all
+    else
+      @sip_accounts = Phone.find(params[:phone_id]).sip_accounts
     end
 
     respond_to do |format|

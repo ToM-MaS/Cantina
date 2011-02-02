084202de84a192f944b81104bbe641edb9a0960f
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-02-02 17:34:48 +0100

    send "404 Not Found" if phone with the given MAC address does not exists

--- app/controllers/manufacturer_aastra_controller.rb
+++ app/controllers/manufacturer_aastra_controller.rb
@@ -4,7 +4,19 @@ class ManufacturerAastraController < ApplicationController
     @mac_address = params[:mac_address]
     mac_address = params[:mac_address].upcase.gsub(/[^A-F0-9]/,'')
     @phone = Phone.where(:mac_address => mac_address).first
-
+    
+    # send "404 Not Found" if phone with the given MAC address does not exists {
+    if @phone.nil?
+      render(
+        :status => 404,
+        :layout => false,
+        :content_type => 'text/plain',
+        :text => "<!-- Phone #{mac_address.inspect} not found. -->",
+      )
+      return
+    end
+    # send "404 Not Found" if phone with the given MAC address does not exists }
+    
     respond_to do |format|
       format.txt
     end
--- app/controllers/manufacturer_snom_controller.rb
+++ app/controllers/manufacturer_snom_controller.rb
@@ -6,6 +6,18 @@ def show
       mac_address = params[:mac_address].upcase.gsub(/[^A-F0-9]/,'')
       @phone = Phone.where(:mac_address => mac_address).first
       
+      # send "404 Not Found" if phone with the given MAC address does not exists {
+      if @phone.nil?
+        render(
+          :status => 404,
+          :layout => false,
+          :content_type => 'text/plain',
+          :text => "<!-- Phone #{mac_address.inspect} not found. -->",
+        )
+        return
+      end
+      # send "404 Not Found" if phone with the given MAC address does not exists }
+      
     end
 
     respond_to do |format|

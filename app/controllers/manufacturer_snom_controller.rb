731dd3a9865914e78e31713307429c2bc319499a
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-02-03 00:36:47 +0100

    Einrückung gefixt.

--- app/controllers/manufacturer_snom_controller.rb
+++ app/controllers/manufacturer_snom_controller.rb
@@ -1,30 +1,31 @@
 class ManufacturerSnomController < ApplicationController
-def show
-  mac_address = params[:mac_address].upcase.gsub(/[^A-F0-9]/,'')
-  @phone = Phone.where(:mac_address => mac_address).first
-  
-  if ! @phone.nil?
-    @phone.provisioning_log_entries.create(:succeeded => true, :memo => "Phone got config")
-    @phone.update_attributes(:ip_address => request.remote_ip)
-    respond_to do |format|
-      format.xml 
-    end
-  else
-    respond_to do |format|
-      format.html
-    end 
-  end
-end
-
-
-
-def index
-    mfc = Manufacturer.where(:ieee_name => "SNOM Technology AG").first
-    @phones = mfc ? mfc.phones : []
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @phones }
-    end
-  end
+	
+	def show
+		mac_address = params[:mac_address].upcase.gsub(/[^A-F0-9]/,'')
+		@phone = Phone.where(:mac_address => mac_address).first
+		
+		if ! @phone.nil?
+			@phone.provisioning_log_entries.create(:succeeded => true, :memo => "Phone got config")
+			@phone.update_attributes(:ip_address => request.remote_ip)
+			
+			respond_to { |format|
+				format.xml 
+			}
+		else
+			respond_to { |format|
+				format.html
+			}
+		end
+	end
+	
+	def index
+		mfc = Manufacturer.where(:ieee_name => "SNOM Technology AG").first
+		@phones = mfc ? mfc.phones : []
+		
+		respond_to { |format|
+			format.html # index.html.erb
+			format.xml  { render :xml => @phones }
+		}
+	end
+	
 end
--- app/controllers/manufacturer_tiptel_controller.rb
+++ app/controllers/manufacturer_tiptel_controller.rb
@@ -1,29 +1,31 @@
 class ManufacturerTiptelController < ApplicationController
-def show
-  
-  mac_address = params[:mac_address].upcase.gsub(/[^A-F0-9]/,'')
-  @phone = Phone.where(:mac_address => mac_address).first
-  
-  if ! @phone.nil?
-    @phone.provisioning_log_entries.create(:succeeded => true, :memo => "Phone got config")
-    @phone.update_attributes(:ip_address => request.remote_ip)
-    respond_to do |format|
-      format.txt
-    end 
-  else
-    respond_to do |format|
-      format.html
-    end 
-  end
-end
-
-def index
-    mfc = Manufacturer.where(:ieee_name => "XIAMEN YEALINK NETWORK TECHNOLOGY CO.,LTD").first
-    @phones = mfc ? mfc.phones : []
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @phones }
-    end
-  end
+	
+	def show
+		mac_address = params[:mac_address].upcase.gsub(/[^A-F0-9]/,'')
+		@phone = Phone.where(:mac_address => mac_address).first
+		
+		if ! @phone.nil?
+			@phone.provisioning_log_entries.create(:succeeded => true, :memo => "Phone got config")
+			@phone.update_attributes(:ip_address => request.remote_ip)
+			
+			respond_to { |format|
+				format.txt
+			}
+		else
+			respond_to { |format|
+				format.html
+			}
+		end
+	end
+	
+	def index
+		mfc = Manufacturer.where(:ieee_name => "XIAMEN YEALINK NETWORK TECHNOLOGY CO.,LTD").first
+		@phones = mfc ? mfc.phones : []
+		
+		respond_to { |format|
+			format.html # index.html.erb
+			format.xml  { render :xml => @phones }
+		}
+	end
+	
 end
